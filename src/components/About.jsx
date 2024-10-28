147c93e57ddb3e5082e3c7a88666e88a6cdbe1e9
Katie Lundell <katie.lundell@outlook.com>
2024-10-28 09:59:13 -0600

    Finish draft of about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,20 +3,21 @@ import "./About.css"
 
 function About() {
   return (
-    <div className='container-100vh-100vw' style={{ position: 'relative', zIndex: 1, }} >
+    <div className='container-100vh-100vw' style={{ position: 'relative', zIndex: 1,}} >
 
 
 
-      <div className='flex-column flexbasis-60vw project-details' style={{border:"2px solid green"}}>
-        <h2 className='project-name'>Chemistry Tutoring Web App</h2>
-        <p className='project-desc'>Elevate your chemistry studies with our innovative web app. Designed to empower students of all levels, our platform offers access to a professional chemistry tutor and comprehensive interactive quizzes to solidify your understanding of fundamental chemistry concepts.</p>
+      <div className='flex-column flexbasis-60vw about-details' >
+
+        <p className='about-desc'>I am Katie Lundell, a computational chemist turned full-stack web developer. With a Ph.D. in Computational Chemistry, I bring a strong foundation in scientific problem-solving to the world of web development.</p>
+        <p className='about-desc'>I am passionate about creating innovative and user-friendly web applications, particularly in the fields of weather, food science, and STEM education. I am eager to collaborate with STEM educators and businesses seeking a skilled web developer.</p>
       </div>
 
 
 
-      <div className='flex-column flexbasis-40vw'>
+      <div className='flex-column flexbasis-30vw'>
         <h1 id='about' className='title-box '>About Me</h1>
-        <img className='about-img' src="./images/KLundell.jpg" />
+        <img className='about-img' src="./images/KLundell-cropped.jpg" />
         <div className='bg-decor'></div>
       </div>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,9 +12,17 @@ function Projects() {
           <div className='bg-decor'></div>
       </div>
 
-        <div className='flex-column flexbasis-40vw project-details'>
+        <div className='flex-column flexbasis-40vw project-details' style={{position:"relative"}}>
           <h2 className='project-name'>Chemistry Tutoring Web App</h2>
           <p className='project-desc'>Elevate your chemistry studies with our innovative web app. Designed to empower students of all levels, our platform offers access to a professional chemistry tutor and comprehensive interactive quizzes to solidify your understanding of fundamental chemistry concepts.</p>
+          <div className='postgres-logo logo-container'>
+            <img className='logo' src='./images/postgres-logo.svg'/>
+            <h3 className='logo-name'>PostgreSQL</h3>
+          </div>
+          <div className='react-logo logo-container'>
+            <img className='logo' src='./images/react-logo.svg'/>
+            <h3 className='logo-name'>React</h3>
+          </div>
         </div>
 
 
